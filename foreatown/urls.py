2470f4759bd1a60b17be9e145dd91862cd21bafd
pd0203 <45405912+pd0203@users.noreply.github.com>
2022-12-08 02:06:42 +0900

    Add: name for every API in foreatown.urls

--- foreatown/urls.py
+++ foreatown/urls.py
@@ -2,14 +2,14 @@ from django.urls import path, include
 from foreatown.views import GatherRoomAPI, GatherRoomReservationAPI, GatherRoomReviewAPI 
 
 urlpatterns = [
-    path("gather-room", GatherRoomAPI.as_view({"post": "create"})),
-    path("gather-room/<int:id>", GatherRoomAPI.as_view({"get": "retrieve", "patch": "partial_update", "delete": "destroy"})),
-    path("gather-room/list", GatherRoomAPI.as_view({"get": "list"})),
-    path("gather-room/list/<int:gather_room_category_id>", GatherRoomAPI.as_view({"get": "list"})), 
-    path("gather-room/mylist/<int:user_id>", GatherRoomAPI.as_view({"get": "my_list"})),   
-    path("gather-room/reservation", GatherRoomReservationAPI.as_view({"post": "create"})),
-    path("gather-room/reservation/list", GatherRoomReservationAPI.as_view({"get": "list"})),
-    path("gather-room/reservation/<int:reservation_id>", GatherRoomReservationAPI.as_view({"delete": "destroy"})),
-    path("gather-room/review", GatherRoomReviewAPI.as_view({"post": "create"})), 
-    path("gather-room/review/list", GatherRoomReviewAPI.as_view({"get": "list"})) 
+    path("gather-room", GatherRoomAPI.as_view({"post": "create"}), name="post_gather_room"),
+    path("gather-room/<int:id>", GatherRoomAPI.as_view({"get": "retrieve", "patch": "partial_update", "delete": "destroy"}), name="create_update_delete_gather_room"),
+    path("gather-room/list", GatherRoomAPI.as_view({"get": "list"}), name="get_gather_room_list"),
+    path("gather-room/list/<int:gather_room_category_id>", GatherRoomAPI.as_view({"get": "list"}), name="get_gather_room_list_by_category"), 
+    path("gather-room/mylist/<int:user_id>", GatherRoomAPI.as_view({"get": "my_list"}), name="get_my_gather_room_list"),   
+    path("gather-room/reservation", GatherRoomReservationAPI.as_view({"post": "create"}), name="post_gather_room_reservation"),
+    path("gather-room/reservation/list", GatherRoomReservationAPI.as_view({"get": "list"}), name="get_gather_room_reservation_list"),
+    path("gather-room/reservation/<int:reservation_id>", GatherRoomReservationAPI.as_view({"delete": "destroy"}), name="delete_gather_room_reservation"),
+    path("gather-room/review", GatherRoomReviewAPI.as_view({"post": "create"}), name="post_gather_room_review"), 
+    path("gather-room/review/list", GatherRoomReviewAPI.as_view({"get": "list"}), name="get_gather_room_review_list") 
 ]
