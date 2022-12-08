09e9245845dca343022b958e872b962149584313
pd0203 <45405912+pd0203@users.noreply.github.com>
2022-12-08 13:51:49 +0900

    Del: .history on github

--- .history/nofilter/admin_20221025180159.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Category, Post, PostFile, Comment, Recomment
-
-admin.site.register(Category)
-admin.site.register(Post)
-admin.site.register(PostFile)
-admin.site.register(Comment)
-admin.site.register(Recomment)
--- .history/users/admin_20221024122621.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import User, Nofilter_highschool, Nofilter_highschool_rating, Nofilter_user_info
-# Register your models here.
-
-admin.site.register(User)
-admin.site.register(Nofilter_highschool)
-admin.site.register(Nofilter_highschool_rating)
-admin.site.register(Nofilter_user_info)
